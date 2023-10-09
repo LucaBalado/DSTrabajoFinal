49d4fdc0acf594e53332054a51d7b53cf2e4e1e9
Luca Balado <lucabalado@gmail.com>
2023-10-09 11:44:41 -0300

    Actividad 4

---
+++ tp_final_Musica_Balado/blog.py
@@ -0,0 +1,20 @@
+from flask import (
+Blueprint, flash, g, redirect, render_template, request, url_for
+)
+from werkzeug.exceptions import abort
+
+from tp_final_Musica_Balado.db import get_db
+
+bp = Blueprint('blog', __name__)
+
+@bp.route('/')
+def index():
+    db = get_db()
+    canciones = db.execute(
+        """SELECT t.name AS Canciones, title AS Disco, ar.name AS Artista, g.name AS Genero
+        FROM tracks t JOIN albums a ON t.AlbumId = a.AlbumId
+        JOIN artists ar ON ar.ArtistId = a.ArtistId
+        JOIN genres g ON g.GenreId = t.GenreId
+        ORDER BY t.name DESC"""
+    ).fetchall()
+    return render_template('blog/index.html', canciones=canciones)
